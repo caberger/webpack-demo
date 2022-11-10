260886c515d4cb204b6c842ec977e0765a397fce
caberger <christian@aberger.at>
2022-11-10 12:46:47 +0100

    map and distinct

--- src/components/user/user-table-component.ts
+++ src/components/user/user-table-component.ts
@@ -3,6 +3,7 @@ import { USER_SELECTED_EVENT } from "."
 import { User } from "../../model/user"
 import userService from "../../user-service"
 import store from "../../model/store"
+import { distinctUntilChanged, map } from "rxjs"
 
 const tableTemplate = html`
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -26,9 +27,14 @@ class UserTableComponent extends HTMLElement {
     connectedCallback() {
         console.log("connected usertable")
         userService.fetchAll()
-        store.subscribe(model => {
-            this.render(model.users)
-        })
+        store
+            .pipe(
+                map(model => model.users),
+                distinctUntilChanged()
+            )
+            .subscribe(users => {
+                this.render(users)
+            })
     }
     private render(users: Array<User>) {
         render(tableTemplate, this.shadowRoot)

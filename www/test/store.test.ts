3b5697fdc5adb2bbd8503b05d0dfa0285d624441
Christian Aberger <qrzbrz@yahoo.de>
2023-02-02 15:09:53 +0100

    add unit tests

--- www/test/store.test.ts
+++ www/test/store.test.ts
@@ -1,23 +1,33 @@
-import {suite, test } from "./utility"
+//import {suite, test, should } from "./utility"
+import { suite, test } from '@testdeck/mocha';
 import store, { setUsers } from "../src/model/store"
 import { map } from "rxjs"
 import { User } from "../src/model/user"
+import * as chai from "chai"
 
 @suite
 export class StoreTest {
-
+    private users: User[]
     before() {
         console.log("setup")
+        this.users = [{id: 1, name: "John Doe"}]
     }
-    @test 'store should call subscribe function when users change'() {
+    @test 'store should call subscribe function twice when users change'() {
+        let responses = Array<User[]>()
         store
             .pipe(
                 map(model => model.users)
             )
-            .subscribe(users => console.log("users changed", users))
+            .subscribe(users => {
+                responses.push(users);
+                console.log("users changed", users, responses)
+            })
 
-        const users: User[] = [{id: 1, name: "John Doe"}]
-        setUsers(users)
+        setUsers(this.users)
+        chai.expect(responses).to.have.lengthOf(2)
+        chai.expect(responses[0]).to.have.lengthOf(0)
+        chai.expect(responses[1][0].name).to.equal("John Doe")
+        console.log("test ended", responses)
     }
 }
 

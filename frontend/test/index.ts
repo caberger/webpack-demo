2bfcb3f348a41deba40fa7cfe92c78c2e6066c14
Christian Aberger <c.aberger@htl-perg.ac.at>
2023-02-10 19:27:23 +0100

    selenium tests

--- frontend/test/index.ts
+++ frontend/test/index.ts
@@ -1,7 +1,18 @@
-import { Builder } from "selenium-webdriver"
-console.log("hello world")
+import { Builder, By, WebDriver } from "selenium-webdriver"
+import { strict as assert } from "node:assert"
+import { expect } from "chai"
 
-const URL = "http://https://www.htl-leonding.at/"
-
-
-
+const URL = "https://www.htl-leonding.at/"
+describe("always succeed test", function() {
+    let driver: WebDriver
+    before(async function() {
+        this.timeout(10000)
+        driver = await new Builder().forBrowser('chrome').build()
+        await driver.get(URL)
+    })
+    it("should have hauptmenue", async function() {
+        const menu = await driver.findElement(By.id("hauptmenue"))
+        expect(menu).to.be.not.null
+        await driver.quit()
+    })
+})

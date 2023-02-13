b8e8f2478c93f694a63a869674eb835e6e0db202
Christian Aberger <qrzbrz@yahoo.de>
2023-02-13 11:42:47 +0100

    use common root find function

--- frontend/test/user-table-test.ts
+++ frontend/test/user-table-test.ts
@@ -2,14 +2,14 @@ import { Builder, By, WebDriver, WebElement } from "selenium-webdriver"
 import { expect } from "chai"
 
 const URL = "http://localhost:4200/"
-describe("App Component Tests", function() {
+describe("User-Table Component Tests", function() {
     let driver: WebDriver
     beforeEach(async function() {
         this.timeout(10000)
         driver = await new Builder().forBrowser('chrome').build()
         await driver.get(URL)
     })
-    this.afterEach(async function() {
+    afterEach(async function() {
         await driver.quit()
     })
     it("should have 'User Table' as title given homepage", async function() {
@@ -18,15 +18,30 @@ describe("App Component Tests", function() {
         expect(title).to.equal("User Table")
     })
     it ("should contain Id and Name as table headers", async function() {
-        const app = await driver.findElement(By.css("app-component"))
-        const root = await app.getShadowRoot()
-        const userTable = await root.findElement(By.css("user-table"))
-        const tableRoot = await userTable.getShadowRoot()
+        const tableRoot = await findUserTableShadowRoot(driver)
         const ths = await tableRoot.findElements(By.css("table > thead > tr > th"))
-        const promises = ths.map((th: WebElement) => th.getText())
+        const promises: Array<Promise<string>> = ths.map((th: WebElement) => th.getText())
         const headers = await Promise.all(promises)
         console.log("found headers", headers)
         expect(headers[0]).to.equal("Id")
         expect(headers[1]).to.equal("Name")
     })
+    /*
+    it ("should contain Max Mustermann given table filled with Demo Data", async function() {
+        const root = await findUserTableShadowRoot(driver)
+        const rows = await root.findElements(By.css("table > tbody > tr"))
+        //const names = new Array<String>()
+        //const promises: Array<Promise<WebElement>> = rows.map((row: WebElement) => row.getText())
+        //const 
+
+    })
+    */
 })
+
+async function findUserTableShadowRoot(driver: WebDriver) {
+    const app = await driver.findElement(By.css("app-component"))
+    const root = await app.getShadowRoot()
+    const userTable = await root.findElement(By.css("user-table"))
+    const tableRoot = await userTable.getShadowRoot()
+    return tableRoot
+}

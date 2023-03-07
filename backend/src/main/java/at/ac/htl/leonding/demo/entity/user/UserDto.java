b99560248ec7971aa648b388a1ee1c822a92c0a1
Christian Aberger <qrzbrz@yahoo.de>
2023-03-07 07:45:28 +0100

    documentation

--- backend/src/main/java/at/ac/htl/leonding/demo/entity/user/UserDto.java
+++ backend/src/main/java/at/ac/htl/leonding/demo/entity/user/UserDto.java
@@ -1,15 +1,4 @@
 package at.ac.htl.leonding.demo.entity.user;
 
-// we cannot use java 17 currently, otherwise the jib cannot run it.
-//public record UserDto(int id, String name) {
-//}
-
-public class UserDto {
-    public int id;
-    public String name;
-
-    public UserDto(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+public record UserDto(int id, String name) {
 }

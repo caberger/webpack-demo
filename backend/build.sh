59468ec30dc078e822ee8162db74919e033b40f5
caberger <c.aberger@htl-perg.ac.at>
2023-10-15 11:10:10 +0200

    fix node build

--- backend/build.sh
+++ backend/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 rm -rf target
-mvn clean package
+mvn -B clean package
 mkdir -p target/deploy
 cp target/*-runner.jar target/deploy/
 docker build -t backend -f ./src/main/docker/Dockerfile ./target/deploy
--- frontend/build.sh
+++ frontend/build.sh
@@ -6,7 +6,8 @@ rm -rf ./target
 mkdir -p target/nginx
 
 pushd www
-    npm install & npm run build
+    npm install
+    npm run build
 popd
 
 cp ./docker/default.conf ./target/nginx/
